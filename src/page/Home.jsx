04671c6b404754030736044a1de17c177e5b224c
RominM <rominmanogil.off@gmail.com>
2022-03-28 14:11:42 +0200

    not-found id condition stoping loader is working

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -18,75 +18,69 @@ import Loader from '../components/Loader';
 const Home = () => {
   // get id from url
   const userId = useParams().id;
-  // init user
-
+  
   // init State
   const [loading, setLoading] = useState(true);
   const [activity, setActivity] = useState([]);
   const [average, setAverage] = useState([]);
   const [performance, setPerformance] = useState([]);
   const [keyData, setKeyData] = useState(null);
-
+  
   // get Data after update state
   useEffect(() => {
     const getKeyData = async () => {
       return service.getMainData(userId);
     };
-
+    
     getKeyData()
-      .then((user) => setKeyData(user))
-      .catch((e) => {
-        console.log(e);
+    .then((user) => setKeyData(user))
+    .catch((e) => {
+      console.log(e);
+      setLoading(false);
+    })
+    .finally(() => {
+      setTimeout(() => {
         setLoading(false);
-      });
-  }, []);
-
+      }, 1500);
+    });
+}, []);
+  
   useEffect(() => {
     if (keyData) {
       const getActivity = async () => {
         return service.getUserActivity(userId);
       };
-
+      
       const getAverage = async () => {
         return service.getUserAverage(userId);
       };
-
+      
       const getPerformance = async () => {
         return service.getUserPerformance(userId);
       };
-
+      
       Promise.all([getActivity(), getAverage(), getPerformance()])
-        .then((values) => {
-          setActivity(values[0]);
-          setAverage(values[1]);
-          setPerformance(values[2]);
-        })
-        .catch((e) => {
-          console.log(e);
-        })
-        .finally(() => {
-          setTimeout(() => {
-            setLoading(false);
-          }, 1500);
-        });
+      .then((values) => {
+        setActivity(values[0]);
+        setAverage(values[1]);
+        setPerformance(values[2]);        
+      })
+      .catch((e) => {
+        console.log(e);
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setLoading(false);
+        }, 1500);
+      });
     }
   }, [keyData]);
-
-  console.log(keyData);
-  /*if (
-    !average ||
-    !performance.data ||
-    !keyData ||
-    !activity
-    // ||!dataCurrentUser
-  ) {
-    return null;
-  }*/
-
+  
   // if wrong user URL
   if (loading) {
     return <Loader />;
   }
+  console.log(loading);
   if (keyData === undefined || keyData === null) {
     return <Navigate to="not-found" />;
   }
